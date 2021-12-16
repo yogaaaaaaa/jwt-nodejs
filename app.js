d4216b58b45b96648533496193ef309507929f57
adhipratama <yogaadhi@icloud.com>
2021-12-16 13:52:17 +0700

    fixing headers error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("./config/database").connect();
 const bcrypt = require("bcryptjs/dist/bcrypt");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -47,16 +48,53 @@ app.post("/register", async (req, res) => {
       {
         user_id: user._id,
         email,
-      }.process.env.TOKEN_KEY,
+      },
+      process.env.TOKEN_KEY,
       {
-        expiresIn: "2h",
+        expiresIn: "1d",
       }
     );
+    //save user token
+    user.token = token;
+
+    res.status(200).json(user);
   } catch (err) {
     console.log(err);
   }
 });
 
-app.post("/login", (req, res) => {});
+app.post("/login", async (req, res) => {
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+     return res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   first_name: { type: String, default: null },
-  las_name: { type: String, default: null },
+  last_name: { type: String, default: null },
   email: { type: String, unique: true },
   password: { type: String },
   token: { type: String },
